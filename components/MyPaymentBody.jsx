a9b4a1c92d52d34b35efe9928d1c71bfdea42efe
Azeezjim <azeexjim@gmail.com>
2022-04-16 11:50:49 -1100

    latest commit of the day

--- components/MyPaymentBody.jsx
+++ components/MyPaymentBody.jsx
@@ -14,21 +14,51 @@ function MyPaymentBody() {
       amount: "$1660.00",
       typeData: "Tip",
     },
+    {
+      name: "John Doe",
+      date: "15 march 2022",
+      amount: "$1660.00",
+      typeData: "Tip",
+    },
+    {
+      name: "John Doe",
+      date: "15 march 2022",
+      amount: "$1660.00",
+      typeData: "Tip",
+    },
+    {
+      name: "John Doe",
+      date: "15 march 2022",
+      amount: "$1660.00",
+      typeData: "Tip",
+    },
+    {
+      name: "John Doe",
+      date: "15 march 2022",
+      amount: "$1660.00",
+      typeData: "Tip",
+    },
+    {
+      name: "John Doe",
+      date: "15 march 2022",
+      amount: "$1660.00",
+      typeData: "Tip",
+    }
   ];
   return (
-    <div className="bg-white border drop-shadow-lg width-full rounded-lg">
+    <div className="bg-white border drop-shadow-lg width-full rounded-lg space-y-3">
       <div className="grid grid-cols-4 justify-items-center	text-[13px] text-lightPlayRed font-bold">
         <p>Paid to</p>
         <p>Date</p>
         <p>Amount</p>
         <p>Type</p>
       </div>
-      {/* <hr /> */}
+      <hr />
       {/* <div> */}
-        {paymentDetails.map((payment, index) => {
-          <div className="grid grid-cols-4 justify-items-center	text-[13px]" key={index}>
-            <div>
-              <p>{payment.name}</p>
+        {paymentDetails.map((payment, index) => (
+          <div className="space-y-2" key={index}>
+          <div className="grid grid-cols-4 justify-items-center	text-[13px]">
+            <div className="flex justify-center space-x-2">
               <Image
                 src="/images/person9.jpg"
                 alt="Picture of the author"
@@ -36,12 +66,16 @@ function MyPaymentBody() {
                 height={20}
                 className="rounded-full"
               />
-            </div>
+              <p>{payment.name}</p>
+            </div >
+            
             <p>{payment.date}</p>
             <p>{payment.amount}</p>
             <p>{payment.typeData}</p>
-          </div>;
-        })}
+          </div>
+          <hr />
+          </div>
+  ))}
       {/* </div> */}
     </div>
   );
