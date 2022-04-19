d7070393cc7d47dc03506ceb0ed830fba5c48c84
Azeezjim <azeexjim@gmail.com>
2022-04-18 23:27:35 -1100

    latest commit of the day

---
+++ components/AddBankBody.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function AddBankBody() {
+  return (
+    <div>
+      <div className="w-[100%] h=[50%] border shadow-lg rounded-md ">
+        <form action="">
+          
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default AddBankBody
