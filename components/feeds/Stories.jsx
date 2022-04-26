6f4c66e1727cb49bc85034979cc68576ecc6d698
ore291 <oreoluwapadonu@gmail.com>
2022-04-26 10:00:02 -0700

    started stories

--- components/feeds/Stories.jsx
+++ components/feeds/Stories.jsx
@@ -1,10 +1,138 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect, useCallback } from "react";
 import Story from "./Story";
 import { useSelector } from "react-redux";
+import StorySliderLoader from "./StorySliderLoader";
 import { FaChevronCircleLeft, FaChevronCircleRight } from "react-icons/fa";
+import { PrevButton, NextButton } from "./EmblaButtons";
+import useEmblaCarousel from "embla-carousel-react";
 
 const Stories = () => {
-  const stories = useSelector(state => state.creators.creators)
+  const [viewportRef, embla] = useEmblaCarousel({
+    dragFree: true,
+    containScroll: "trimSnaps",
+  });
+  const [prevBtnEnabled, setPrevBtnEnabled] = useState(false);
+  const [nextBtnEnabled, setNextBtnEnabled] = useState(false);
+
+  const scrollPrev = useCallback(() => embla && embla.scrollPrev(), [embla]);
+  const scrollNext = useCallback(() => embla && embla.scrollNext(), [embla]);
+  const onSelect = useCallback(() => {
+    if (!embla) return;
+    setPrevBtnEnabled(embla.canScrollPrev());
+    setNextBtnEnabled(embla.canScrollNext());
+  }, [embla]);
+
+  useEffect(() => {
+    if (!embla) return;
+    embla.on("select", onSelect);
+    onSelect();
+  }, [embla, onSelect]);
+
+  const [renderSliderModal, setRenderSliderModal] = useState(false);
+
+  const [selectedSliderIndex, setSelectedSliderIndex] = useState(0);
+
+  const [sliderData, setSliderData] = useState({});
+
+  var settings = {
+    dots: false,
+    infinite: false,
+    speed: 500,
+    responsive: [
+      {
+        breakpoint: 2048,
+        settings: {
+          slidesToShow: 9.5,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 1920,
+        settings: {
+          slidesToShow: 9.5,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 8,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 6,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 5.5,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 376,
+        settings: {
+          slidesToShow: 1.75,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  const SliderModalToggle = (status, index, story) => {
+    setRenderSliderModal(status);
+    setSelectedSliderIndex(index);
+    setSliderData(story);
+  };
+
+  // useEffect(() => {
+  //   props.dispatch(fetchStoriesStart());
+  // }, []);
+
+  // useEffect(() => {
+  //   if (SliderModalToggle) {
+  //     $("#storiesSliderModal").modal("show");
+  //   } else {
+  //     $("#storiesSliderModal").modal("hide");
+  //   }
+  // }, [renderSliderModal]);
+
+  // my own start
+
+  const stories = useSelector((state) => state.creators.creators);
   const storiesRef = useRef(null);
   const [showLeft, setShowLeft] = useState(false);
   const [showRight, setShowRight] = useState(true);
@@ -24,8 +152,8 @@ const Stories = () => {
     }
   };
   return (
-    <div className=" mb-5">
-      <div
+    <div className=" mt-3 md:mt-0 mb-5 p-1">
+      {/* <div
         onScroll={onScroll}
         ref={storiesRef}
         className="flex items-baseline space-x-1 md:space-x-2 py-4 px-1 md:p-4 shadow-md bg-white border-gray-300 
@@ -43,31 +171,31 @@ const Stories = () => {
             img={story.image}
           />
         ))}
-      </div>
-      {/* <div className="hidden lg:flex lg:absolute top-0 p-4 w-full h-full  justify-between z-10 items-center">
-        <button
-          onClick={() => {
-            storiesRef.current.scrollLeft -= 300;
-          }}
-        >
-          <FaChevronCircleLeft
-            className={`bg-gray-600 text-white rounded-full w-6 h-6 cursor-pointer drop-shadow-lg filter ${
-              showLeft ? "visible" : "invisible"
-            }`}
-          />
-        </button>
-        <button
-          onClick={() => {
-            storiesRef.current.scrollLeft += 300;
-          }}
-        >
-          <FaChevronCircleRight
-            className={`bg-gray-600 rounded-full text-white w-6 h-6 cursor-pointer drop-shadow-lg filter ${
-              showRight ? "visible" : "invisible"
-            }`}
-          />
-        </button>
       </div> */}
+      <div className="embla">
+        <div className="embla__viewport" ref={viewportRef}>
+          <div className="embla__container">
+            <Story
+              username={"Create new story"}
+              img={"/profile_avatar_full.jpg"}
+              isYou={true}
+              className="embla__slide1"
+            />
+            {stories.map((story, index) => (
+              <div className="embla__slide1" key={index}>
+                <Story
+                  key={index}
+                  username={story.username}
+                  img={story.image}
+                  className=""
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+        <PrevButton onClick={scrollPrev} enabled={prevBtnEnabled} />
+        <NextButton onClick={scrollNext} enabled={nextBtnEnabled} />
+      </div>
     </div>
   );
 };
