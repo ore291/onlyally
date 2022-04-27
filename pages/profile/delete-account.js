c7a8b253c9cfad9e36a15d246a227a8ab7a73b4e
Azeezjim <azeexjim@gmail.com>
2022-04-27 05:08:28 -1100

    first commit of the day

--- pages/profile/delete-account.js
+++ pages/profile/delete-account.js
@@ -1,6 +1,7 @@
 import ProfileNavBar from "../../components/ProfileNavBar.jsx";
 import TransactionHistoryBody from "../../components/TransactionHistoryBody.jsx";
 import TransactinHistoryTop from "../../components/TransactinHistoryTop.jsx";
+import DeleteAccountBody from "../../components/Profile/DeleteAccountBody.jsx";
 
 function TrasctionHistory() {
   return (
@@ -8,9 +9,8 @@ function TrasctionHistory() {
       <div className="flex p-2 rounded shadow">
         <ProfileNavBar className="w-24 mb-8" />
         <div className=" w-[90%] px-1 mx-auto pt-3 mr-24  mt-12 bg-white rounded">
-          <TransactinHistoryTop />
-          <p className="text-gray-400">TRANSACTION</p>
-          <TransactionHistoryBody className="w-[90%]"/>
+          <p className="font-bold text-center">Delete Account</p>
+          <DeleteAccountBody className="w-[90%]"/>
         </div>
       </div>
     </div>
