851662418b1cb1c1045e264821d23baeb697b59d
ore291 <oreoluwapadonu@gmail.com>
2022-05-13 12:42:22 +0100

    .

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -11,15 +11,15 @@ var localStorage = require("localStorage");
 // const DeviceHelper = require('node-device-detector/helper');
 // 'use strict';
  
-var rootCas = require('ssl-root-cas').create();
+// var rootCas = require('ssl-root-cas').create();
  
-rootCas
-  .addFile('pages/api/auth/ssl/ss_bundle.pem')
-  .addFile('pages/api/auth/ssl/ss_cert.pem')
-  ;
+// rootCas
+//   .addFile('pages/api/auth/ssl/ss_bundle.pem')
+//   .addFile('pages/api/auth/ssl/ss_cert.pem')
+//   ;
  
-// will work with all https requests will all libraries (i.e. request.js)
-require('https').globalAgent.options.ca = rootCas;
+// // will work with all https requests will all libraries (i.e. request.js)
+// require('https').globalAgent.options.ca = rootCas;
 
 
 export default NextAuth({
