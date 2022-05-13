2bfd66607bf296c64f3b6fc8481245d63c1779e2
ore291 <oreoluwapadonu@gmail.com>
2022-05-13 20:32:44 +0100

    .

--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
 const webpack = require("webpack");
 
+'use strict';
+
+
+var rootCas = require('ssl-root-cas').create();
+ 
+// rootCas
+//   .addFile('pages/api/auth/ssl/ss_bundle.pem')
+//   .addFile('pages/api/auth/ssl/ss_cert.pem')
+//   ;
+ 
+// will work with all https requests will all libraries (i.e. request.js)
+require('https').globalAgent.options.ca = rootCas;
+
 module.exports = {
   
   reactStrictMode: true,
