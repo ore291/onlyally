051f0d0159268c8eeaa84017bcf40909ec3da92d
ore291 <oreoluwapadonu@gmail.com>
2022-05-13 20:27:50 +0100

    .

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -14,10 +14,10 @@ var localStorage = require("localStorage");
 
 var rootCas = require('ssl-root-cas').create();
  
-rootCas
-  .addFile('pages/api/auth/ssl/ss_bundle.pem')
-  .addFile('pages/api/auth/ssl/ss_cert.pem')
-  ;
+// rootCas
+//   .addFile('pages/api/auth/ssl/ss_bundle.pem')
+//   .addFile('pages/api/auth/ssl/ss_cert.pem')
+//   ;
  
 // will work with all https requests will all libraries (i.e. request.js)
 require('https').globalAgent.options.ca = rootCas;
