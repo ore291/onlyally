2e720c633571665a1fd96722d326229334ee6797
ore291 <oreoluwapadonu@gmail.com>
2022-08-16 22:52:27 +0100

    .

--- components/junk.js
+++ components/junk.js
@@ -1,4 +1,4 @@
-<Transition.Root show={open} as={Fragment}>
+{/* <Transition.Root show={open} as={Fragment}>
 <Dialog
   as="div"
   className="fixed z-10 inset-0 overflow-y-auto"
@@ -21,56 +21,56 @@
     </Transition.Child>
 
     {/* This element is to trick the browser into centering the modal contents. */}
-    <span
-      className="hidden md:inline-block md:align-middle md:h-screen"
-      aria-hidden="true"
-    >
-      &#8203;
-    </span>
-    <Transition.Child
-      as={Fragment}
-      enter="ease-out duration-300"
-      enterFrom="opacity-0 translate-y-4 md:translate-y-0 md:scale-95"
-      enterTo="opacity-100 translate-y-0 md:scale-100"
-      leave="ease-in duration-200"
-      leaveFrom="opacity-100 translate-y-0 md:scale-100"
-      leaveTo="opacity-0 translate-y-4 md:translate-y-0 md:scale-95"
-    >
-      <div className="flex text-base text-left transform transition w-full md:inline-block md:max-w-2xl md:px-4 md:my-8 md:align-middle lg:max-w-4xl">
-        <div className="w-full relative flex items-center bg-white px-4 pt-14 pb-8 overflow-hidden shadow-2xl sm:px-6 sm:pt-8 md:p-6 lg:p-8">
-          <button
-            type="button"
-            className="absolute top-4 right-4 text-gray-400 hover:text-gray-500 sm:top-8 sm:right-6 md:top-6 md:right-6 lg:top-8 lg:right-8"
-            onClick={() => setOpen(false)}
-          >
-            <span className="sr-only">Close</span>
-            <MdClose className="h-6 w-6" aria-hidden="true" />
-          </button>
+    // <span
+    //   className="hidden md:inline-block md:align-middle md:h-screen"
+    //   aria-hidden="true"
+    // >
+    //   &#8203;
+    // </span>
+    // <Transition.Child
+    //   as={Fragment}
+    //   enter="ease-out duration-300"
+    //   enterFrom="opacity-0 translate-y-4 md:translate-y-0 md:scale-95"
+    //   enterTo="opacity-100 translate-y-0 md:scale-100"
+    //   leave="ease-in duration-200"
+    //   leaveFrom="opacity-100 translate-y-0 md:scale-100"
+    //   leaveTo="opacity-0 translate-y-4 md:translate-y-0 md:scale-95"
+    // >
+    //   <div className="flex text-base text-left transform transition w-full md:inline-block md:max-w-2xl md:px-4 md:my-8 md:align-middle lg:max-w-4xl">
+    //     <div className="w-full relative flex items-center bg-white px-4 pt-14 pb-8 overflow-hidden shadow-2xl sm:px-6 sm:pt-8 md:p-6 lg:p-8">
+    //       <button
+    //         type="button"
+    //         className="absolute top-4 right-4 text-gray-400 hover:text-gray-500 sm:top-8 sm:right-6 md:top-6 md:right-6 lg:top-8 lg:right-8"
+    //         onClick={() => setOpen(false)}
+    //       >
+    //         <span className="sr-only">Close</span>
+    //         <MdClose className="h-6 w-6" aria-hidden="true" />
+    //       </button>
 
-          <div className="w-full grid grid-cols-1 gap-y-8 gap-x-6 items-start sm:grid-cols-12 lg:gap-x-8">
-            <div className="aspect-w-2 aspect-h-3 rounded-lg bg-gray-100 overflow-hidden sm:col-span-4 lg:col-span-5">
-              <img
-                src={product.productInfo.picture}
-                alt={product.productInfo.name}
-                className="object-center object-cover"
-              />
-            </div>
-            <div className="sm:col-span-8 lg:col-span-7">
-              <h2 className="text-2xl font-extrabold text-gray-900 sm:pr-12">
-                {product.productInfo.name}
-              </h2>
+    //       <div className="w-full grid grid-cols-1 gap-y-8 gap-x-6 items-start sm:grid-cols-12 lg:gap-x-8">
+    //         <div className="aspect-w-2 aspect-h-3 rounded-lg bg-gray-100 overflow-hidden sm:col-span-4 lg:col-span-5">
+    //           <img
+    //             src={product.productInfo.picture}
+    //             alt={product.productInfo.name}
+    //             className="object-center object-cover"
+    //           />
+    //         </div>
+    //         <div className="sm:col-span-8 lg:col-span-7">
+    //           <h2 className="text-2xl font-extrabold text-gray-900 sm:pr-12">
+    //             {product.productInfo.name}
+    //           </h2>
 
-              <section
-                aria-labelledby="information-heading"
-                className="mt-2"
-              >
-                <h3 id="information-heading" className="sr-only">
-                  Product information
-                </h3>
+    //           <section
+    //             aria-labelledby="information-heading"
+    //             className="mt-2"
+    //           >
+    //             <h3 id="information-heading" className="sr-only">
+    //               Product information
+    //             </h3>
 
-                <p className="text-2xl text-gray-900">
-                  {product.productInfo.user_product_price_formatted}
-                </p>
+    //             <p className="text-2xl text-gray-900">
+    //               {product.productInfo.user_product_price_formatted}
+    //             </p> */}
 
                 {/* Reviews */}
                 {/* <div className="mt-6">
@@ -101,7 +101,7 @@
                     </a>
                   </div>
                 </div> */}
-              </section>
+              {/* </section>
 
               <section
                 aria-labelledby="options-heading"
@@ -111,7 +111,7 @@
                   Product options
                 </h3>
 
-                <form>
+                <form> */}
                   {/* Colors */}
                   {/* <div>
                     <h4 className="text-sm text-gray-900 font-medium">
@@ -244,7 +244,7 @@
                     </RadioGroup>
                   </div> */}
 
-                  <button
+                  {/* <button
                     type="button"
                     className="mt-6 w-full bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                   >
@@ -259,4 +259,4 @@
     </Transition.Child>
   </div>
 </Dialog>
-</Transition.Root>
+</Transition.Root> */}
