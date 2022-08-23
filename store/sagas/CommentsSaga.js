5b20f64f559e9c44e1d8fb5b29ee5d00f5cb15a2
ore291 <oreoluwapadonu@gmail.com>
2022-08-23 20:40:24 +0100

    .

--- store/sagas/CommentsSaga.js
+++ store/sagas/CommentsSaga.js
@@ -2,109 +2,137 @@ import { call, select, put, takeLatest, all } from "redux-saga/effects";
 
 import api from "../../Environment";
 
-
-
 import {
-    deleteCommentFailure,
-    deleteCommentSuccess,
-    fetchCommentsFailure,
-    fetchCommentsSuccess,
-    saveCommentFailure,
-    saveCommentSuccess,
-    fetchCommentRepliesFailure,
-    fetchCommentRepliesSuccess,
-    saveCommentRepliesFailure,
-    saveCommentRepliesSuccess,
-    fetchCommentsStart
-  } from "../slices/commentsSlice";
+  deleteCommentFailure,
+  deleteCommentSuccess,
+  fetchCommentsFailure,
+  fetchCommentsSuccess,
+  saveCommentFailure,
+  saveCommentSuccess,
+  fetchCommentRepliesFailure,
+  fetchCommentRepliesSuccess,
+  saveCommentRepliesFailure,
+  saveCommentRepliesSuccess,
+  fetchCommentsStart,
+} from "../slices/commentsSlice";
 
-import {notify} from 'reapop';
+import { errorLogoutCheck } from "../slices/errorSlice";
 
+import { notify } from "reapop";
 
-  function* fetchCommentsAPI() {
-    try {
-      const inputData = yield select((state) => state.comments.comments.inputData);
-      const response = yield api.postMethod({action:"post_comments", object: inputData});
-      if (response.data.success) {
-        
-        yield put(fetchCommentsSuccess(response.data.data));
-      } else {
-        yield put(fetchCommentsFailure( response.data.error.error));
-        
-        // yield put(checkLogoutStatus(response.data));
-        yield put(notify({message:  response.data.error.error,status:"error"}));
-      }
-    } catch (error) {
-      yield put(fetchCommentsFailure(error));
-    yield put(notify({message: error.message,status:"error"}));
-    }
-  }
+function* fetchCommentsAPI() {
+  try {
+    const inputData = yield select(
+      (state) => state.comments.comments.inputData
+    );
+    const response = yield api.postMethod({
+      action: "post_comments",
+      object: inputData,
+    });
+    if (response.data.success) {
+      yield put(fetchCommentsSuccess(response.data.data));
+    } else {
+      yield put(fetchCommentsFailure(response.data.error.error));
 
-  function* saveCommentAPI() {
-    try {
-      const inputData = yield select(
-        (state) => state.comments.saveComment.inputData
+      yield put(errorLogoutCheck(response.data));
+      yield put(
+        notify({ message: response.data.error.error, status: "error" })
       );
-      const response = yield api.postMethod({action:"post_comments_save",object: inputData});
-      if (response.data.success) {
-        yield put(saveCommentSuccess(response.data.data));
-        yield put(fetchCommentsStart(inputData))
-        yield put(notify({ message: response.data.message, status: 'success' }))
-      } else {
-        yield put(saveCommentFailure( response.data.error.error));
-        // yield put(checkLogoutStatus(response.data));
-        yield put(notify({message:  response.data.error.error,status:"error"}));
-      }
-    } catch (error) {
-      yield put(saveCommentFailure(error));
-      yield put(notify({message: error.message,status:"error"}));
     }
+  } catch (error) {
+    yield put(fetchCommentsFailure(error));
+    yield put(notify({ message: error.message, status: "error" }));
   }
+}
 
-  function* fetchCommentRepliesAPI() {
-    try {
-      const inputData = yield select((state) => state.comments.commentReplies.inputData);
-      const response = yield api.postMethod({action:"post_comment_replies",object:inputData });
-      if (response.data.success) {
-        yield put(fetchCommentRepliesSuccess(response.data.data));
-      } else {
-        yield put(fetchCommentRepliesFailure( response.data.error.error));
-        // yield put(checkLogoutStatus(response.data));
-        yield put(notify({message:  response.data.error.error,status:"error"}));
-      }
-    } catch (error) {
-      yield put(fetchCommentRepliesFailure(error));
-      yield put(notify({message: error.message,status:"error"}));
-    }
-  }
-  
-  function* saveCommentReplyAPI() {
-    try {
-      const inputData = yield select(
-        (state) => state.comment.saveCommentReply.inputData
+function* saveCommentAPI() {
+  try {
+    const inputData = yield select(
+      (state) => state.comments.saveComment.inputData
+    );
+    const response = yield api.postMethod({
+      action: "post_comments_save",
+      object: inputData,
+    });
+    if (response.data.success) {
+      yield put(saveCommentSuccess(response.data.data));
+      yield put(fetchCommentsStart(inputData));
+      yield put(notify({ message: response.data.message, status: "success" }));
+    } else {
+      yield put(saveCommentFailure(response.data.error));
+      yield put(errorLogoutCheck(response.data));
+      yield put(
+        notify({ message: response.data.error.error, status: "error" })
       );
-      const response = yield api.postMethod({action:"post_comments_replies_save",object:inputData});
-      if (response.data.success) {
-        yield put(saveCommentRepliesSuccess(response.data.data));
-        yield put(notify({ message: response.data.message, status: 'success' }));
-        
-      } else {
-        yield put(saveCommentRepliesFailure( response.data.error.error));
-        // yield put(checkLogoutStatus(response.data));
-        yield put(notify({message:  response.data.error.error,status:"error"}));
-      }
-    } catch (error) {
-      yield put(saveCommentRepliesFailure(error));
-      yield put(notify({message: error.message,status:"error"}));
     }
+  } catch (error) {
+    yield put(saveCommentFailure(error));
+    yield put(notify({ message: error.message, status: "error" }));
   }
+}
 
-
-  export default function* pageSaga() {
-    yield all([yield takeLatest("comments/fetchCommentsStart", fetchCommentsAPI)]);
-    yield all([yield takeLatest("comments/fetchCommentRepliesStart", fetchCommentRepliesAPI)]);
-    yield all([yield takeLatest("comments/saveCommentStart", saveCommentAPI)]);
-    yield all([yield takeLatest("comments/saveCommentRepliesStart", saveCommentReplyAPI)]);
-    // yield all([yield takeLatest(DELETE_COMMENT_START, deleteCommentAPI)]);
+function* fetchCommentRepliesAPI() {
+  try {
+    const inputData = yield select(
+      (state) => state.comments.commentReplies.inputData
+    );
+    const response = yield api.postMethod({
+      action: "post_comment_replies",
+      object: inputData,
+    });
+    if (response.data.success) {
+      yield put(fetchCommentRepliesSuccess(response.data.data));
+    } else {
+      yield put(fetchCommentRepliesFailure(response.data.error));
+      yield put(errorLogoutCheck(response.data));
+      yield put(
+        notify({ message: response.data.error, status: "error" })
+      );
+    }
+  } catch (error) {
+    yield put(fetchCommentRepliesFailure(error));
+    yield put(notify({ message: error.message, status: "error" }));
   }
-  
+}
+
+function* saveCommentReplyAPI() {
+  try {
+    const inputData = yield select(
+      (state) => state.comments.saveCommentReply.inputData
+    );
+    const response = yield api.postMethod({
+      action: "post_comments_replies_save",
+      object: inputData,
+    });
+    if (response.data.success) {
+      yield put(saveCommentRepliesSuccess(response.data.data));
+      yield put(notify({ message: response.data.message, status: "success" }));
+    } else {
+      yield put(saveCommentRepliesFailure(response.data.error));
+      yield put(errorLogoutCheck(response.data));
+      yield put(
+        notify({ message: response.data.error.error, status: "error" })
+      );
+    }
+  } catch (error) {
+    yield put(saveCommentRepliesFailure(error));
+    yield put(notify({ message: error.message, status: "error" }));
+  }
+}
+
+export default function* pageSaga() {
+  yield all([
+    yield takeLatest("comments/fetchCommentsStart", fetchCommentsAPI),
+  ]);
+  yield all([
+    yield takeLatest(
+      "comments/fetchCommentRepliesStart",
+      fetchCommentRepliesAPI
+    ),
+  ]);
+  yield all([yield takeLatest("comments/saveCommentStart", saveCommentAPI)]);
+  yield all([
+    yield takeLatest("comments/saveCommentRepliesStart", saveCommentReplyAPI),
+  ]);
+  // yield all([yield takeLatest(DELETE_COMMENT_START, deleteCommentAPI)]);
+}
