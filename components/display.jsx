4401d4301e5d441b990c32e6a043ccfcbe6ddce2
coded-coder <paulambrose5002@gmail.com>
2022-09-01 15:15:26 +0100

    worked on the userprofile and added a hover effect to the right side bar

---
+++ components/display.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Image from "next/image";
+import { useState } from "react";
+const Display = ({ i, user, handleChatUser }) => {
+  const [seeChild, setSeeChild] = useState(false);
+  return (
+    <div className="relative">
+      <div className=" w-fit h-14 relative cursor-pointer " key={i}>
+        <Image
+          onClick={(event) => {
+            console.log(user.to_user_id);
+            handleChatUser(event, user.to_user_id);
+          }}
+          onMouseEnter={() => setSeeChild(true)}
+          onMouseLeave={() => setSeeChild(false)}
+          src={user.to_userpicture}
+          alt="side-img"
+          width="50px"
+          height="50px"
+          className=" rounded-full"
+        />
+      </div>
+      {seeChild && (
+        <div className="animate-display w-[150px] h-[60px] py-[5px] px-[30px] absolute cursor-pointer z-50 bg-white grid grid-cols-2 left-[-200px] top-[0] rounded-xl drop-shadow-[5px_5px_5px_rgba(0,0,0,0.5)]">
+          <Image
+            src={user.to_userpicture}
+            alt="side-img"
+            width="50px"
+            height="50px"
+            className=" rounded-[50%] "
+          />
+          <p className="font-bold mt-2 ml-2 ">{user.to_username}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Display;
