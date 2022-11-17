f820ad8860ff4c0514bf0756fe0b4f75b23fcbc3
ore291 <oreoluwapadonu@gmail.com>
2022-11-17 13:57:26 +0100

    .

--- components/Profile/PulseImage.jsx
+++ components/Profile/PulseImage.jsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const PulseImage = ({ image }) => {
   return (
-    <Link href="/stories" passHref>
+    <Link href="/user-stories" passHref>
       <div className="relative h-36 w-36 2xl:w-52 2xl:h-52 rounded-3xl cursor-pointer">
         <div className="absolute rounded-3xl inset-0 border-indigo-700 border-[3px] transition delay-1000 animate-pulse" />
         <div className="absolute rounded-3xl inset-[3px] border-indigo-400 border-4 animate-[pulse_2s_ease-in-out_infinite]" />
